92c98d0edc7527a9af64347d0b6c2ef0db10b4bf
code.retriever@gmail.com <code.retriever@gmail.com>
2014-12-20 09:43:50 -0500

    initial commit

---
+++ ExampleEight/src/com/exampleeight/EmployeeIdGenerator.java
@@ -0,0 +1,18 @@
+package com.exampleeight;
+
+/**
+ * The class that is responsible
+ * to generate employee Ids new employees.
+ */
+public final class EmployeeIdGenerator {
+
+    /**
+     * Static method that is responsible to generate
+     * the next employee id.
+     * @return The next employee id.
+     * Currently this method throws UnsupportedOperationException.
+     */
+    public final static int getNextId() {
+        throw new UnsupportedOperationException();
+    }
+}
