0e4397235d58617f3350e5000197db7e32cdc9f2
code.retriever@gmail.com <code.retriever@gmail.com>
2014-12-20 09:16:55 -0500

    initial commit

---
+++ ExampleThree/test/com/examplethree/EmployeeTest.java
@@ -0,0 +1,34 @@
+package com.examplethree;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
+
+import com.examplethree.Employee;
+
+/**
+ * The class that holds all unit tests for
+ * the Employee class.
+ *  
+ */
+public class EmployeeTest {
+
+    @Test()
+    public void shouldNotDoAnythingIfEmployeeWasSaved() {
+        Employee employee = PowerMockito.mock(Employee.class);
+        PowerMockito.doNothing().when(employee).save();
+        try {
+            employee.save();
+        } catch(Exception e) {
+            Assert.fail("Should not have thrown an exception");
+        }
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldThrowAnExceptionIfEmployeeWasNotSaved() {
+        Employee employee = PowerMockito.mock(Employee.class);
+        PowerMockito.doThrow(new IllegalStateException()).when(employee).save();
+
+        employee.save();
+    }
+}
