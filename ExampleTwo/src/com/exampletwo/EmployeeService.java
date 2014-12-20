0b7b66b8d799b589b568fc45a9bece675955c916
code.retriever@gmail.com <code.retriever@gmail.com>
2014-12-20 09:14:53 -0500

    initial commit

---
+++ ExampleTwo/src/com/exampletwo/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.exampletwo;
+
+/**
+ * This class is responsible to handle the CRUD
+ * operations on the Employee objects.
+ *  
+ */
+public class EmployeeService {
+
+    /**
+     * This method is responsible to return
+     * the count of employees in the system.
+     * Currently this method does nothing but
+     * simply throws an exception.
+     * @return Total number of employees in the system.
+     */
+    public int getEmployeeCount() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ ExampleTwo/test/com/exampletwo/EmployeeControllerTest.java
@@ -0,0 +1,32 @@
+package com.exampletwo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+
+import com.exampletwo.EmployeeController;
+import com.exampletwo.EmployeeService;
+
+/**
+ * The class that holds all unit tests for
+ * the EmployeeController class.
+ *  
+ */
+public class EmployeeControllerTest {
+
+    @Test
+    public void shouldReturnCountOfEmployeesFromTheService() {
+        //Creating a mock using the PowerMockito.mock method for the EmployeeService class.
+        EmployeeService mock = PowerMockito.mock(EmployeeService.class);
+
+        //Next statement essentially says that when getEmployeeCount method
+        //is called on the mocked EmployeeService instance, return 10.
+        PowerMockito.when(mock.getEmployeeCount()).thenReturn(10);
+
+        EmployeeController employeeController = new EmployeeController(mock);
+        Assert.assertEquals(10, employeeController.getEmployeeCount());
+    }
+}
