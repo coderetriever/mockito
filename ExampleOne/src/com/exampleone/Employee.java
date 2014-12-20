888de13cce361f6ec389469e0097de813e30ad40
code.retriever@gmail.com <code.retriever@gmail.com>
2014-12-20 08:18:19 -0500

    initial commit

---
+++ ExampleOne/src/com/exampleone/Employee.java
@@ -0,0 +1,8 @@
+package com.exampleone;
+
+/**
+ * This is a model class that will hold
+ * properties specific to an employee in the system.
+ */
+public class Employee {
+}
---
+++ ExampleOne/src/com/exampleone/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.exampleone;
+
+/**
+ * This class is responsible to handle the CRUD
+ * operations on the Employee objects.
+ */
+public class EmployeeService {
+
+    /**
+     * This method is responsible to return
+     * the count of employees in the system.
+     * Currently this method does nothing but
+     * simply throws an exception.
+     * @return Total number of employees in the system.
+     */
+    public int getEmployeeCount() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * The method that will save
+     * the employee instance to the DB.
+     * @param employee instance to save.
+     */
+    public void save(Employee employee) {
+        throw new UnsupportedOperationException();
+    }
+}
